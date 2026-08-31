3d3004743452030642fdd917ae7c04cacea0f2ef
Volgin-alex <volginaleksej1@gmail.com>
2026-08-31 17:21:30 +0300

    add Ztest

--- sites/lib/format.ts
+++
@@ -1,7 +0,0 @@
-export function formatMoney(cents: number) {
-  return new Intl.NumberFormat("ru-RU", {
-    style: "currency",
-    currency: "RUB",
-    maximumFractionDigits: 0,
-  }).format(cents / 100);
-}
